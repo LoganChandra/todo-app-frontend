7a3be5585125b5b207d4b5589d3d66ac724ad6ae
Logan Chandrasegaran <logan.chandra@ecloudvalley.com>
2022-10-15 15:02:15 +0800

    fix: added navbar with title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <va-navbar color="primary" shape class="mb-2">
+      <template #left>
+        <va-navbar-item>
+          <p class="font-semibold text-2xl">TODO or NOT TODO</p>
+        </va-navbar-item>
+      </template>
+    </va-navbar>
     <div class="contents">
       <router-view />
     </div>
